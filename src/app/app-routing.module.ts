0474e8456956bc6555a4498f3619574470c3577b
tonykobe8 <tonykobe8@gmail.com>
2020-09-10 23:43:27 +0200

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule,Routes } from '@angular/router';
-
-
+import { Routes, RouterModule } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 
 
-const appRoutes: Routes = [{path :'employee' ,component:EmployeeComponent}]; 
+
+
+const routes: Routes = [ 
+   
+    { path: 'employee', component: EmployeeComponent }
+ ];
+
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule],
-  declarations: []
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
