fd83a441cbe8b7b823d3bf959694c949c164e385
tonykobe8 <tonykobe8@gmail.com>
2020-09-10 23:46:59 +0200

    2 files modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { EmployeeComponent } from './employee/employee.component';
 
-
-
-
 const routes: Routes = [ 
    
     { path: 'employee', component: EmployeeComponent }
