d9791da60ef78f3faed3227abe9863ce41bb10f4
tonykobe8 <tonykobe8@gmail.com>
2020-09-10 22:12:04 +0200

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { RouterModule,Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
-import { AppRoutingModule } from './app-routing.module';
 import { EmployeeComponent } from './employee/employee.component';
 
 
 @NgModule({
-  imports:      [ BrowserModule,AppRoutingModule  ],
-  declarations: [ EmployeeComponent],
+  imports:      [ BrowserModule,AppRoutingModule],
+  declarations: [ AppComponent,EmployeeComponent],
   bootstrap :[AppComponent]
 })
 export class AppModule { }
