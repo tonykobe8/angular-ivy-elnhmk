d92ca685a0570cf4eb53b6cb68bbb0fe976575fa
tonykobe8 <tonykobe8@gmail.com>
2020-09-11 12:18:29 +0200

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,11 +11,12 @@ import { HelloComponent } from './hello.component';
 import { AppRoutingModule } from './app-routing.module';
 import { EmployeesComponent } from './employees/employees.component';
 import { DepartmentsComponent } from './departments/departments.component';
+import { MenuHeaderComponent } from './menu-header/menu-header.component';
 
 
 @NgModule({
   imports:      [ BrowserModule,AppRoutingModule],
-  declarations: [ AppComponent, EmployeesComponent, DepartmentsComponent],
+  declarations: [ AppComponent, EmployeesComponent, DepartmentsComponent, MenuHeaderComponent],
   bootstrap :[AppComponent]
 })
 export class AppModule { }
